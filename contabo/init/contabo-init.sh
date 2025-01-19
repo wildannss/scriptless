5fa0ee30f447dcfb60dd9c88e38978b92cd237d4
wildannss <wildan.syarif1021@gmail.com>
2025-01-19 14:27:26 +0700

    remove upgrade

--- contabo/init/contabo-init.sh
+++ contabo/init/contabo-init.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get update
 sudo apt-get clean -y
-sudo apt-get upgrade -y
 [ -e '/etc/needrestart/needrestart.conf' ] && (printf "\n\$nrconf{restart} = 'a';" >> /etc/needrestart/needrestart.conf)
 wget https://software.virtualmin.com/gpl/scripts/virtualmin-install.sh
 sudo sh virtualmin-install.sh -f -v
@@ -28,7 +27,6 @@ sudo apt install -y software-properties-common
 printf "enter" | sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get clean -y
-sudo apt-get upgrade -y
 sudo apt install -y python3.13
 apt install -y python3-pip
 sudo apt install -y python-is-python3
@@ -37,12 +35,10 @@ sudo apt-get install -y build-essential
 printf 'enter' | sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get clean -y
-sudo apt-get upgrade -y
 sudo apt-get install -y php8.4
 sudo apt install php8.2-common php8.4-mysql php8.4-xml php8.4-xmlrpc php8.4-curl php8.4-gd php8.4-imagick php8.4-cli php8.4-dev php8.4-imap php8.4-mbstring php8.4-opcache php8.4-soap php8.4-zip php8.4-intl -y
 sudo apt-get update
 sudo apt-get clean -y
-sudo apt-get upgrade -y
 sudo apt install ntpdate -y
 sudo ntpdate -s time.nist.gov
 sudo timedatectl set-timezone UTC
@@ -64,11 +60,8 @@ sudo addgroup sftp
 sudo useradd -m sftpuser -g sftp
 sudo chmod 700 /home/sftpuser
 sudo apt-get clean -y
-sudo apt-get upgrade -y
 echo "" >> /etc/crontab
 echo "@daily root sudo apt-get update" >> /etc/crontab
-echo "" >> /etc/crontab
-echo "@daily root sudo apt-get upgrade -y" >> /etc/crontab
 echo "" >> /etc/crontab
 echo "@daily root /root/bekap_r.sh" >> /etc/crontab
 echo "" >> /etc/crontab
@@ -88,7 +81,6 @@ command
 sudo apt autoremove -y
 sudo apt-get update
 sudo apt-get clean -y
-sudo apt-get upgrade -y
 mkdir -p /root/gunbot.my.id
 wget https://gunthy.org/downloads/gunthy_linux.zip -P /root/gunbot.my.id
 cd /root/gunbot.my.id
